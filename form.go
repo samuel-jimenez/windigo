e30db63bef4b7c222ecfa3a01a605c02649cf0c5
samuel-jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2025-05-09 11:56:31 -0500

    chore: coalesce control names

--- form.go
+++ form.go
@@ -6,8 +6,9 @@
 package windigo
 
 import (
-	"github.com/samuel-jimenez/windigo/w32"
 	"unsafe"
+
+	"github.com/samuel-jimenez/windigo/w32"
 )
 
 type LayoutManager interface {
@@ -27,11 +28,11 @@ type Form struct {
 }
 
 func NewCustomForm(parent Controller, exStyle int, dwStyle uint) *Form {
-	fm := new(Form)
+	control := new(Form)
 
 	RegClassOnlyOnce("windigo_Form")
 
-	fm.isForm = true
+	control.isForm = true
 
 	if exStyle == 0 {
 		exStyle = w32.WS_EX_CONTROLPARENT | w32.WS_EX_APPWINDOW
@@ -41,80 +42,80 @@ func NewCustomForm(parent Controller, exStyle int, dwStyle uint) *Form {
 		dwStyle = w32.WS_OVERLAPPEDWINDOW
 	}
 
-	fm.hwnd = CreateWindow("windigo_Form", parent, uint(exStyle), dwStyle)
-	fm.parent = parent
+	control.hwnd = CreateWindow("windigo_Form", parent, uint(exStyle), dwStyle)
+	control.parent = parent
 
 	// this might fail if icon resource is not embedded in the binary
 	if ico, err := NewIconFromResource(GetAppInstance(), uint16(AppIconID)); err == nil {
-		fm.SetIcon(0, ico)
+		control.SetIcon(0, ico)
 	}
 
 	// This forces display of focus rectangles, as soon as the user starts to type.
-	w32.SendMessage(fm.hwnd, w32.WM_CHANGEUISTATE, w32.UIS_INITIALIZE, 0)
+	w32.SendMessage(control.hwnd, w32.WM_CHANGEUISTATE, w32.UIS_INITIALIZE, 0)
 
-	RegMsgHandler(fm)
+	RegMsgHandler(control)
 
-	fm.SetFont(DefaultFont)
-	fm.SetText("Form")
-	return fm
+	control.SetFont(DefaultFont)
+	control.SetText("Form")
+	return control
 }
 
 func NewForm(parent Controller) *Form {
-	fm := new(Form)
+	control := new(Form)
 
 	RegClassOnlyOnce("windigo_Form")
 
-	fm.isForm = true
-	fm.hwnd = CreateWindow("windigo_Form", parent, w32.WS_EX_CONTROLPARENT|w32.WS_EX_APPWINDOW, w32.WS_OVERLAPPEDWINDOW)
-	fm.parent = parent
+	control.isForm = true
+	control.hwnd = CreateWindow("windigo_Form", parent, w32.WS_EX_CONTROLPARENT|w32.WS_EX_APPWINDOW, w32.WS_OVERLAPPEDWINDOW)
+	control.parent = parent
 
 	// this might fail if icon resource is not embedded in the binary
 	if ico, err := NewIconFromResource(GetAppInstance(), uint16(AppIconID)); err == nil {
-		fm.SetIcon(0, ico)
+		control.SetIcon(0, ico)
 	}
 
 	// This forces display of focus rectangles, as soon as the user starts to type.
-	w32.SendMessage(fm.hwnd, w32.WM_CHANGEUISTATE, w32.UIS_INITIALIZE, 0)
+	w32.SendMessage(control.hwnd, w32.WM_CHANGEUISTATE, w32.UIS_INITIALIZE, 0)
 
-	RegMsgHandler(fm)
+	RegMsgHandler(control)
 
-	fm.SetFont(DefaultFont)
-	fm.SetText("Form")
-	return fm
+	control.SetFont(DefaultFont)
+	control.SetText("Form")
+	return control
 }
 
-func (fm *Form) SetLayout(mng LayoutManager) {
-	fm.layoutMng = mng
+func (control *Form) SetLayout(mng LayoutManager) {
+	control.layoutMng = mng
 }
 
 // UpdateLayout refresh layout.
-func (fm *Form) UpdateLayout() {
-	if fm.layoutMng != nil {
-		fm.layoutMng.Update()
+func (control *Form) UpdateLayout() {
+	if control.layoutMng != nil {
+		control.layoutMng.Update()
 	}
 }
 
-func (fm *Form) NewMenu() *Menu {
+func (control *Form) NewMenu() *Menu {
 	hMenu := w32.CreateMenu()
 	if hMenu == 0 {
 		panic("failed CreateMenu")
 	}
-	m := &Menu{hMenu: hMenu, hwnd: fm.hwnd}
-	if !w32.SetMenu(fm.hwnd, hMenu) {
+	m := &Menu{hMenu: hMenu, hwnd: control.hwnd}
+	if !w32.SetMenu(control.hwnd, hMenu) {
 		panic("failed SetMenu")
 	}
 	return m
 }
 
-func (fm *Form) DisableIcon() {
-	windowInfo := getWindowInfo(fm.hwnd)
+func (control *Form) DisableIcon() {
+	windowInfo := getWindowInfo(control.hwnd)
 	frameless := windowInfo.IsPopup()
 	if frameless {
 		return
 	}
-	exStyle := w32.GetWindowLong(fm.hwnd, w32.GWL_EXSTYLE)
-	w32.SetWindowLong(fm.hwnd, w32.GWL_EXSTYLE, uint32(exStyle|w32.WS_EX_DLGMODALFRAME))
-	w32.SetWindowPos(fm.hwnd, 0, 0, 0, 0, 0,
+	exStyle := w32.GetWindowLong(control.hwnd, w32.GWL_EXSTYLE)
+	w32.SetWindowLong(control.hwnd, w32.GWL_EXSTYLE, uint32(exStyle|w32.WS_EX_DLGMODALFRAME))
+	w32.SetWindowPos(control.hwnd, 0, 0, 0, 0, 0,
 		uint(
 			w32.SWP_FRAMECHANGED|
 				w32.SWP_NOMOVE|
@@ -123,110 +124,110 @@ func (fm *Form) DisableIcon() {
 	)
 }
 
-func (fm *Form) Maximise() {
-	w32.ShowWindow(fm.hwnd, w32.SW_MAXIMIZE)
+func (control *Form) Maximise() {
+	w32.ShowWindow(control.hwnd, w32.SW_MAXIMIZE)
 }
 
-func (fm *Form) Minimise() {
-	w32.ShowWindow(fm.hwnd, w32.SW_MINIMIZE)
+func (control *Form) Minimise() {
+	w32.ShowWindow(control.hwnd, w32.SW_MINIMIZE)
 }
 
-func (fm *Form) Restore() {
-	w32.ShowWindow(fm.hwnd, w32.SW_RESTORE)
+func (control *Form) Restore() {
+	w32.ShowWindow(control.hwnd, w32.SW_RESTORE)
 }
 
 // Public methods
-func (fm *Form) Center() {
+func (control *Form) Center() {
 
-	windowInfo := getWindowInfo(fm.hwnd)
+	windowInfo := getWindowInfo(control.hwnd)
 	frameless := windowInfo.IsPopup()
 
-	info := getMonitorInfo(fm.hwnd)
+	info := getMonitorInfo(control.hwnd)
 	workRect := info.RcWork
 	screenMiddleW := workRect.Left + (workRect.Right-workRect.Left)/2
 	screenMiddleH := workRect.Top + (workRect.Bottom-workRect.Top)/2
 	var winRect *w32.RECT
 	if !frameless {
-		winRect = w32.GetWindowRect(fm.hwnd)
+		winRect = w32.GetWindowRect(control.hwnd)
 	} else {
-		winRect = w32.GetClientRect(fm.hwnd)
+		winRect = w32.GetClientRect(control.hwnd)
 	}
 	winWidth := winRect.Right - winRect.Left
 	winHeight := winRect.Bottom - winRect.Top
 	windowX := screenMiddleW - (winWidth / 2)
 	windowY := screenMiddleH - (winHeight / 2)
-	w32.SetWindowPos(fm.hwnd, w32.HWND_TOP, int(windowX), int(windowY), int(winWidth), int(winHeight), w32.SWP_NOSIZE)
+	w32.SetWindowPos(control.hwnd, w32.HWND_TOP, int(windowX), int(windowY), int(winWidth), int(winHeight), w32.SWP_NOSIZE)
 }
 
-func (fm *Form) Fullscreen() {
-	if fm.isFullscreen {
+func (control *Form) Fullscreen() {
+	if control.isFullscreen {
 		return
 	}
 
-	fm.previousWindowStyle = uint32(w32.GetWindowLongPtr(fm.hwnd, w32.GWL_STYLE))
-	monitor := w32.MonitorFromWindow(fm.hwnd, w32.MONITOR_DEFAULTTOPRIMARY)
+	control.previousWindowStyle = uint32(w32.GetWindowLongPtr(control.hwnd, w32.GWL_STYLE))
+	monitor := w32.MonitorFromWindow(control.hwnd, w32.MONITOR_DEFAULTTOPRIMARY)
 	var monitorInfo w32.MONITORINFO
 	monitorInfo.CbSize = uint32(unsafe.Sizeof(monitorInfo))
 	if !w32.GetMonitorInfo(monitor, &monitorInfo) {
 		return
 	}
-	if !w32.GetWindowPlacement(fm.hwnd, &fm.previousWindowPlacement) {
+	if !w32.GetWindowPlacement(control.hwnd, &control.previousWindowPlacement) {
 		return
 	}
-	w32.SetWindowLong(fm.hwnd, w32.GWL_STYLE, fm.previousWindowStyle & ^uint32(w32.WS_OVERLAPPEDWINDOW))
-	w32.SetWindowPos(fm.hwnd, w32.HWND_TOP,
+	w32.SetWindowLong(control.hwnd, w32.GWL_STYLE, control.previousWindowStyle & ^uint32(w32.WS_OVERLAPPEDWINDOW))
+	w32.SetWindowPos(control.hwnd, w32.HWND_TOP,
 		int(monitorInfo.RcMonitor.Left),
 		int(monitorInfo.RcMonitor.Top),
 		int(monitorInfo.RcMonitor.Right-monitorInfo.RcMonitor.Left),
 		int(monitorInfo.RcMonitor.Bottom-monitorInfo.RcMonitor.Top),
 		w32.SWP_NOOWNERZORDER|w32.SWP_FRAMECHANGED)
-	fm.isFullscreen = true
+	control.isFullscreen = true
 }
 
-func (fm *Form) UnFullscreen() {
-	if !fm.isFullscreen {
+func (control *Form) UnFullscreen() {
+	if !control.isFullscreen {
 		return
 	}
-	w32.SetWindowLong(fm.hwnd, w32.GWL_STYLE, fm.previousWindowStyle)
-	w32.SetWindowPlacement(fm.hwnd, &fm.previousWindowPlacement)
-	w32.SetWindowPos(fm.hwnd, 0, 0, 0, 0, 0,
+	w32.SetWindowLong(control.hwnd, w32.GWL_STYLE, control.previousWindowStyle)
+	w32.SetWindowPlacement(control.hwnd, &control.previousWindowPlacement)
+	w32.SetWindowPos(control.hwnd, 0, 0, 0, 0, 0,
 		w32.SWP_NOMOVE|w32.SWP_NOSIZE|w32.SWP_NOZORDER|w32.SWP_NOOWNERZORDER|w32.SWP_FRAMECHANGED)
-	fm.isFullscreen = false
+	control.isFullscreen = false
 }
 
 // IconType: 1 - ICON_BIG; 0 - ICON_SMALL
-func (fm *Form) SetIcon(iconType int, icon *Icon) {
+func (control *Form) SetIcon(iconType int, icon *Icon) {
 	if iconType > 1 {
 		panic("IconType is invalid")
 	}
-	w32.SendMessage(fm.hwnd, w32.WM_SETICON, uintptr(iconType), uintptr(icon.Handle()))
+	w32.SendMessage(control.hwnd, w32.WM_SETICON, uintptr(iconType), uintptr(icon.Handle()))
 }
 
-func (fm *Form) EnableMaxButton(b bool) {
-	ToggleStyle(fm.hwnd, b, w32.WS_MAXIMIZEBOX)
+func (control *Form) EnableMaxButton(b bool) {
+	ToggleStyle(control.hwnd, b, w32.WS_MAXIMIZEBOX)
 }
 
-func (fm *Form) EnableMinButton(b bool) {
-	ToggleStyle(fm.hwnd, b, w32.WS_MINIMIZEBOX)
+func (control *Form) EnableMinButton(b bool) {
+	ToggleStyle(control.hwnd, b, w32.WS_MINIMIZEBOX)
 }
 
-func (fm *Form) EnableSizable(b bool) {
-	ToggleStyle(fm.hwnd, b, w32.WS_THICKFRAME)
+func (control *Form) EnableSizable(b bool) {
+	ToggleStyle(control.hwnd, b, w32.WS_THICKFRAME)
 }
 
-func (fm *Form) EnableDragMove(_ bool) {
-	//fm.isDragMove = b
+func (control *Form) EnableDragMove(_ bool) {
+	//control.isDragMove = b
 }
 
-func (fm *Form) EnableTopMost(b bool) {
+func (control *Form) EnableTopMost(b bool) {
 	tag := w32.HWND_NOTOPMOST
 	if b {
 		tag = w32.HWND_TOPMOST
 	}
-	w32.SetWindowPos(fm.hwnd, tag, 0, 0, 0, 0, w32.SWP_NOMOVE|w32.SWP_NOSIZE)
+	w32.SetWindowPos(control.hwnd, tag, 0, 0, 0, 0, w32.SWP_NOMOVE|w32.SWP_NOSIZE)
 }
 
-func (fm *Form) WndProc(msg uint32, wparam, lparam uintptr) uintptr {
+func (control *Form) WndProc(msg uint32, wparam, lparam uintptr) uintptr {
 
 	switch msg {
 	case w32.WM_COMMAND:
@@ -234,7 +235,7 @@ func (fm *Form) WndProc(msg uint32, wparam, lparam uintptr) uintptr {
 			// Menu support.
 			actionID := uint16(w32.LOWORD(uint32(wparam)))
 			if action, ok := actionsByID[actionID]; ok {
-				action.onClick.Fire(NewEvent(fm, nil))
+				action.onClick.Fire(NewEvent(control, nil))
 			}
 		}
 	case w32.WM_KEYDOWN:
@@ -246,7 +247,7 @@ func (fm *Form) WndProc(msg uint32, wparam, lparam uintptr) uintptr {
 			shortcut := Shortcut{ModifiersDown(), key}
 			if action, ok := shortcut2Action[shortcut]; ok {
 				if action.Enabled() {
-					action.onClick.Fire(NewEvent(fm, nil))
+					action.onClick.Fire(NewEvent(control, nil))
 				}
 			}
 		}
@@ -258,30 +259,30 @@ func (fm *Form) WndProc(msg uint32, wparam, lparam uintptr) uintptr {
 		return 0
 
 	case w32.WM_SIZE, w32.WM_PAINT:
-		if fm.layoutMng != nil {
-			fm.layoutMng.Update()
+		if control.layoutMng != nil {
+			control.layoutMng.Update()
 		}
 	case w32.WM_GETMINMAXINFO:
-		if fm.minWidth != 0 || fm.maxWidth != 0 || fm.minHeight != 0 || fm.maxHeight != 0 {
-			dpix, dpiy := fm.GetWindowDPI()
+		if control.minWidth != 0 || control.maxWidth != 0 || control.minHeight != 0 || control.maxHeight != 0 {
+			dpix, dpiy := control.GetWindowDPI()
 
 			DPIScaleX := dpix / 96.0
 			DPIScaleY := dpiy / 96.0
 
 			mmi := (*w32.MINMAXINFO)(unsafe.Pointer(lparam))
-			if fm.minWidth > 0 && fm.minHeight > 0 {
-				mmi.PtMinTrackSize.X = int32(fm.minWidth * int(DPIScaleX))
-				mmi.PtMinTrackSize.Y = int32(fm.minHeight * int(DPIScaleY))
+			if control.minWidth > 0 && control.minHeight > 0 {
+				mmi.PtMinTrackSize.X = int32(control.minWidth * int(DPIScaleX))
+				mmi.PtMinTrackSize.Y = int32(control.minHeight * int(DPIScaleY))
 			}
-			if fm.maxWidth > 0 && fm.maxHeight > 0 {
-				mmi.PtMaxSize.X = int32(fm.maxWidth * int(DPIScaleX))
-				mmi.PtMaxSize.Y = int32(fm.maxHeight * int(DPIScaleY))
-				mmi.PtMaxTrackSize.X = int32(fm.maxWidth * int(DPIScaleX))
-				mmi.PtMaxTrackSize.Y = int32(fm.maxHeight * int(DPIScaleY))
+			if control.maxWidth > 0 && control.maxHeight > 0 {
+				mmi.PtMaxSize.X = int32(control.maxWidth * int(DPIScaleX))
+				mmi.PtMaxSize.Y = int32(control.maxHeight * int(DPIScaleY))
+				mmi.PtMaxTrackSize.X = int32(control.maxWidth * int(DPIScaleX))
+				mmi.PtMaxTrackSize.Y = int32(control.maxHeight * int(DPIScaleY))
 			}
 			return 0
 		}
 	}
 
-	return w32.DefWindowProc(fm.hwnd, msg, wparam, lparam)
+	return w32.DefWindowProc(control.hwnd, msg, wparam, lparam)
 }
