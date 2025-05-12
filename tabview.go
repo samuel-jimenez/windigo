5af92e27e75fe6669ef4616a65655699b89d10f6
samuel-jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2025-05-12 14:36:31 -0500

    chore: coalesce control names

--- tabview.go
+++ tabview.go
@@ -20,24 +20,24 @@ type TabView struct {
 }
 
 func NewTabView(parent Controller) *TabView {
-	tv := new(TabView)
+	control := new(TabView)
 
-	tv.InitControl(w32.WC_TABCONTROL, parent, 0,
+	control.InitControl(w32.WC_TABCONTROL, parent, 0,
 		w32.WS_CHILD|w32.WS_VISIBLE|w32.WS_TABSTOP|w32.WS_CLIPSIBLINGS)
-	RegMsgHandler(tv)
+	RegMsgHandler(control)
 
-	tv.panels = NewMultiPanel(parent)
+	control.panels = NewMultiPanel(parent)
 
-	tv.SetFont(DefaultFont)
-	tv.SetSize(200, 24)
-	return tv
+	control.SetFont(DefaultFont)
+	control.SetSize(200, 24)
+	return control
 }
 
-func (tv *TabView) Panels() *MultiPanel {
-	return tv.panels
+func (control *TabView) Panels() *MultiPanel {
+	return control.panels
 }
 
-func (tv *TabView) tcitemFromPage(panel *Panel) *w32.TCITEM {
+func (control *TabView) tcitemFromPage(panel *Panel) *w32.TCITEM {
 	text := syscall.StringToUTF16(panel.Text())
 	item := &w32.TCITEM{
 		Mask:       w32.TCIF_TEXT,
@@ -47,59 +47,59 @@ func (tv *TabView) tcitemFromPage(panel *Panel) *w32.TCITEM {
 	return item
 }
 
-func (tv *TabView) AddPanel(text string) *Panel {
-	panel := NewPanel(tv.panels)
+func (control *TabView) AddPanel(text string) *Panel {
+	panel := NewPanel(control.panels)
 	panel.SetText(text)
 
-	item := tv.tcitemFromPage(panel)
-	index := tv.panels.Count()
-	idx := int(w32.SendMessage(tv.hwnd, w32.TCM_INSERTITEM, uintptr(index), uintptr(unsafe.Pointer(item))))
+	item := control.tcitemFromPage(panel)
+	index := control.panels.Count()
+	idx := int(w32.SendMessage(control.hwnd, w32.TCM_INSERTITEM, uintptr(index), uintptr(unsafe.Pointer(item))))
 	if idx == -1 {
 		panic("SendMessage(TCM_INSERTITEM) failed")
 	}
 
-	tv.panels.AddPanel(panel)
-	tv.SetCurrent(idx)
+	control.panels.AddPanel(panel)
+	control.SetCurrent(idx)
 	return panel
 }
 
-func (tv *TabView) DeletePanel(index int) {
-	w32.SendMessage(tv.hwnd, w32.TCM_DELETEITEM, uintptr(index), 0)
-	tv.panels.DeletePanel(index)
+func (control *TabView) DeletePanel(index int) {
+	w32.SendMessage(control.hwnd, w32.TCM_DELETEITEM, uintptr(index), 0)
+	control.panels.DeletePanel(index)
 	switch {
-	case tv.panels.Count() > index:
-		tv.SetCurrent(index)
-	case tv.panels.Count() == 0:
-		tv.SetCurrent(0)
+	case control.panels.Count() > index:
+		control.SetCurrent(index)
+	case control.panels.Count() == 0:
+		control.SetCurrent(0)
 	}
 }
 
-func (tv *TabView) Current() int {
-	return tv.panels.Current()
+func (control *TabView) Current() int {
+	return control.panels.Current()
 }
 
-func (tv *TabView) SetCurrent(index int) {
-	if index < 0 || index >= tv.panels.Count() {
+func (control *TabView) SetCurrent(index int) {
+	if index < 0 || index >= control.panels.Count() {
 		panic("invalid index")
 	}
-	if ret := int(w32.SendMessage(tv.hwnd, w32.TCM_SETCURSEL, uintptr(index), 0)); ret == -1 {
+	if ret := int(w32.SendMessage(control.hwnd, w32.TCM_SETCURSEL, uintptr(index), 0)); ret == -1 {
 		panic("SendMessage(TCM_SETCURSEL) failed")
 	}
-	tv.panels.SetCurrent(index)
+	control.panels.SetCurrent(index)
 }
 
-func (tv *TabView) WndProc(msg uint32, wparam, lparam uintptr) uintptr {
+func (control *TabView) WndProc(msg uint32, wparam, lparam uintptr) uintptr {
 	switch msg {
 	case w32.WM_NOTIFY:
 		nmhdr := (*w32.NMHDR)(unsafe.Pointer(lparam))
 
 		switch int32(nmhdr.Code) {
 		case w32.TCN_SELCHANGE:
-			cur := int(w32.SendMessage(tv.hwnd, w32.TCM_GETCURSEL, 0, 0))
-			tv.SetCurrent(cur)
+			cur := int(w32.SendMessage(control.hwnd, w32.TCM_GETCURSEL, 0, 0))
+			control.SetCurrent(cur)
 
-			tv.onSelectedChange.Fire(NewEvent(tv, nil))
+			control.onSelectedChange.Fire(NewEvent(control, nil))
 		}
 	}
-	return w32.DefWindowProc(tv.hwnd, msg, wparam, lparam)
+	return w32.DefWindowProc(control.hwnd, msg, wparam, lparam)
 }
